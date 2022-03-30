c38226198433a61a0cbec40a25af20c49c9f5512
Jared Daniels <danielsj@familysearch.org>
2022-03-29 23:50:37 -0600

    Add some default authors

--- src/main/resources/db/migration/V1__initial_version.sql
+++ src/main/resources/db/migration/V1__initial_version.sql
@@ -32,3 +32,15 @@ CREATE SEQUENCE SEQ_AUTHORS_ID START 1 INCREMENT 1 CACHE 10;
 CREATE INDEX IF NOT EXISTS idx_books_name on books (book_title);
 CREATE INDEX IF NOT EXISTS idx_authors_first_name on authors (author_first_name);
 CREATE INDEX IF NOT EXISTS idx_authors_last_name on authors (author_last_name);
+
+-- Create some authors ... just to have something to use
+INSERT INTO authors (author_first_name, author_last_name) values ('JK', 'Rowling');
+INSERT INTO authors (author_first_name, author_last_name) values ('Nora', 'Roberts');
+INSERT INTO authors (author_first_name, author_last_name) values ('James', 'Patterson');
+INSERT INTO authors (author_first_name, author_last_name) values ('Dr.', 'Seuss');
+INSERT INTO authors (author_first_name, author_last_name) values ('John', 'Grisham');
+INSERT INTO authors (author_first_name, author_last_name) values ('Stephanie', 'Meyer');
+INSERT INTO authors (author_first_name, author_last_name) values ('Dan', 'Brown');
+INSERT INTO authors (author_first_name, author_last_name) values ('Nicholas', 'Sparks');
+INSERT INTO authors (author_first_name, author_last_name) values ('Janet', 'Evanovich');
+INSERT INTO authors (author_first_name, author_last_name) values ('Jeff', 'Kinney');
